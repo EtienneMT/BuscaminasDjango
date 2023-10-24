8c738c5ad9e504419c53752eb1bf57f70090fee8
EtienneMT <emartor534@iesmartinezm.es>
2023-10-24 12:50:38 +0200

    cambios de estilo

--- buscamina/views.py
+++ buscamina/views.py
@@ -13,8 +13,6 @@ def tablero(request):
         if form.is_valid():
             filas = form.cleaned_data['filas']
             columnas = form.cleaned_data['columnas']
-            # Aquí puedes generar el tablero con las filas y columnas especificadas
-            # y pasar los datos a la plantilla.
             tablero = crear_tablero(filas, columnas)
             return render(request, 'buscamina/crear_tablero.html', {'tablero': tablero})
     else:
@@ -23,7 +21,5 @@ def tablero(request):
 
 
 def crear_tablero(filas, columnas):
-    # Aquí puedes generar el tablero como una lista de listas o en el formato que necesites.
-    # Por ejemplo, puedes usar un bucle para crear una lista de listas de celdas vacías.
     tablero = [['' for _ in range(columnas)] for _ in range(filas)]
     return tablero
